5a949f3e5d57ba51b6b7dc35ffbe496601e286a3
github username <github email address>
2019-05-19 22:16:41 -0700

    Done.

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -1,15 +1,11 @@
 require 'rails_helper'
- 
+
 describe 'new post' do
-  it 'ensures that the form route works with the /new action' do
+  it 'ensures that the form route works with new action' do
     visit new_post_path
     expect(page.status_code).to eq(200)
   end
 
-  require 'rails_helper'
- 
-describe 'new post' do
- 
   it "displays a new post form that redirects to the index page, which then contains the submitted post's title and description" do
     visit new_post_path
     fill_in 'post_title', with: 'My post title'
@@ -21,5 +17,5 @@ describe 'new post' do
     expect(page).to have_content('My post title')
     expect(page).to have_content('My post description')
   end
-end
+  
 end
